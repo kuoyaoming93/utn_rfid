2c314467cf3ddcc1084b904ea3ca05fd87c5f964
Agustin Miguel Grosso <agrosso@frba.utn.edu.ar>
2017-04-30 17:58:16 -0300

    Testbench Miller

--- VERILOG/Miller Modificado/Miller Modif Deco/tb_mill_modif_demod.v
+++ VERILOG/Miller Modificado/Miller Modif Deco/tb_mill_modif_demod.v
@@ -6,7 +6,7 @@
 --	Se va a ver la salida con un delay de 1 (uno) ETU	--
 ----------------------------------------------------------------*/
 
-//Frecuencia in_clk = 3.39MHz
+//Frecuencia clk = 3.39MHz
 //Frecuencia ETU = 106KHz
 // Entran 32 clock's dentro de cada ETU
 
@@ -15,65 +15,65 @@
 module mill_modif_demod_tb;	//Modulo de test bench
 
 // Entradas del DUT son reg
-  reg in_clk;
+  reg clk;
   reg in_data;
-  reg in_enable;
+  reg in_PoR;
   reg pause;
 //   Las salidas del DUT son wire
   wire out_data;
   
-  mill_modif_demod u1(in_clk, in_enable, in_data, out_data, pause);	//DUT a testear
+  mill_modif_demod u1(clk, in_PoR, in_data, out_data, pause);	//DUT a testear
 //----------------------------------------------------------
 // Inicializo
 // Inicializar todo para que no quede en estados "fantasmas"
     initial
     begin
-      in_clk= 1'b0; 
+      clk= 1'b0; 
       in_data= 1'b0;
-      in_enable=1'b0; 
+      in_PoR=1'b0; 
       pause = 1'b0;
 
       end
 //----------------------------------------------------------
 // Creo un clock de 500khz
     always
-      #1 in_clk = ~in_clk;   // Cada 1 us invierto
+      #1 clk = ~clk;   // Cada 1 us invierto
 	 
 //-----------------------------------------------------------
 
     initial
       begin
-//      forever #10 in_clk = ~in_clk; // generate a clock	
+//      forever #10 clk = ~clk; // generate a clock	
 	#10 repeat(4) #8 in_data = ~in_data;		//10us del arranque, switcheo in_data cada 8us
-	#36 in_enable=1'b1; 				//200us despues de la instruccion anterior habilito el dispositivo
+	#36 in_PoR=1'b1; 				//200us despues de la instruccion anterior habilito el dispositivo
 // 	#16 repeat(4) #16 in_data = ~in_data;		//8us despues de la instruccion anterior switcheo in_data cada 24us
 	in_data = 1'b1;					//Pongo a 1 in_data
 	#32 in_data = ~in_data;				//Switcheo a los 4us in_data
-	pause = ~pause;
-	#32 in_data = ~in_data;				//Switcheo a los 4us in_data
-	pause = ~pause;
-	#64 in_data = ~in_data;				//Switcheo a los 4us in_data
-	pause = ~pause;
-	#32 in_data = ~in_data;				//Switcheo a los 4us in_data
-	pause = ~pause;
-	#64 in_data = ~in_data;				//Switcheo a los 4us in_data
-	pause = ~pause;
-	#32 in_data = ~in_data;				//Switcheo a los 4us in_data
-	pause = ~pause;
-	#32 in_data = ~in_data;				//Switcheo a los 4us in_data
-	pause = ~pause;
-	#32 in_data = ~in_data;				//Switcheo a los 4us in_data
-	pause = ~pause;
+	#28pause = ~pause;
+	#4 in_data = ~in_data;				//Switcheo a los 4us in_data
+	#4pause = ~pause;
+	#60 in_data = ~in_data;				//Switcheo a los 4us in_data
+	#28pause = ~pause;
+	#4 in_data = ~in_data;				//Switcheo a los 4us in_data
+	#4pause = ~pause;
+	#60 in_data = ~in_data;				//Switcheo a los 4us in_data
+	#28pause = ~pause;
+	#4 in_data = ~in_data;				//Switcheo a los 4us in_data
+	#4pause = ~pause;
+	#28 in_data = ~in_data;				//Switcheo a los 4us in_data
+	#28pause = ~pause;
+	#4 in_data = ~in_data;				//Switcheo a los 4us in_data
+	#4pause = ~pause;
 	
-// 	#16 in_enable=1'b0; 	
-// 	#32 in_enable=1'b1; 
+// 	#16 in_PoR=1'b0; 	
+// 	#32 in_PoR=1'b1; 
 //  	#560 repeat(10) #10 in_data = ~in_data;
-	#32 in_enable=1'b0; 				//Deshabilito el dispositivo
+	#32 in_PoR=1'b0; 				//Deshabilito el dispositivo
 	#16 $finish;					//Finalizo la simulación
       end
  
 //     initial	
-//       $monitor("in_clk=%b,in_data=%b,in_enable=%b",in_clk,in_data,in_enable);	//Imprimo en consola los datos
+//       $monitor("clk=%b,in_data=%b,in_PoR=%b",clk,in_data,in_PoR);	//Imprimo en consola los datos
  
     initial
     begin
