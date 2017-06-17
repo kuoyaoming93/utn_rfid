f864a6967190e678dcc668204f1ddcf223d9402d
Agustin Miguel Grosso <agrosso@frba.utn.edu.ar>
2017-06-17 11:19:44 -0300

    Testbench miller modificado 17-06

--- VERILOG/Miller Modificado/Miller Modif Deco/tb_mill_modif_demod.v
+++ VERILOG/Miller Modificado/Miller Modif Deco/tb_mill_modif_demod.v
@@ -15,14 +15,15 @@
 module mill_modif_demod_tb;	//Modulo de test bench
 
 // Entradas del DUT son reg
-  reg clk;
-  reg in_data;
-  reg in_PoR;
-  reg pause;
+  	reg clk;
+  	reg in_data;
+  	reg in_PoR;
+  	reg in_pause;
 //   Las salidas del DUT son wire
-  wire out_data;
+  	wire out_data;
+	wire out_pause;
   
-  mill_modif_demod u1(clk, in_PoR, in_data, out_data, pause);	//DUT a testear
+  	mill_modif_demod u1(clk, in_PoR, in_data, out_data, in_pause, out_pause);	//DUT a testear
 //----------------------------------------------------------
 // Inicializo
 // Inicializar todo para que no quede en estados "fantasmas"
@@ -31,7 +32,7 @@ module mill_modif_demod_tb;	//Modulo de test bench
       clk= 1'b0; 
       in_data= 1'b0;
       in_PoR=1'b0; 
-      pause = 1'b0;
+      in_pause = 1'b0;
 
       end
 //----------------------------------------------------------
@@ -49,26 +50,26 @@ module mill_modif_demod_tb;	//Modulo de test bench
 // 	#16 repeat(4) #16 in_data = ~in_data;		//8us despues de la instruccion anterior switcheo in_data cada 24us
 	in_data = 1'b1;					//Pongo a 1 in_data
 	#32 in_data = ~in_data;				//Switcheo a los 4us in_data
-	#28pause = ~pause;
+	#28in_pause = ~in_pause;
 	#4 in_data = ~in_data;				//Switcheo a los 4us in_data
-	#4pause = ~pause;
+	#4in_pause = ~in_pause;
 	#60 in_data = ~in_data;				//Switcheo a los 4us in_data
-	#28pause = ~pause;
+	#28in_pause = ~in_pause;
 	#4 in_data = ~in_data;				//Switcheo a los 4us in_data
-	#4pause = ~pause;
+	#4in_pause = ~in_pause;
 	#60 in_data = ~in_data;				//Switcheo a los 4us in_data
-	#28pause = ~pause;
+	#28in_pause = ~in_pause;
 	#4 in_data = ~in_data;				//Switcheo a los 4us in_data
-	#4pause = ~pause;
+	#4in_pause = ~in_pause;
 	#28 in_data = ~in_data;				//Switcheo a los 4us in_data
-	#28pause = ~pause;
+	#28in_pause = ~in_pause;
 	#4 in_data = ~in_data;				//Switcheo a los 4us in_data
-	#4pause = ~pause;
+	#4in_pause = ~in_pause;
 	
 // 	#16 in_PoR=1'b0; 	
 // 	#32 in_PoR=1'b1; 
 //  	#560 repeat(10) #10 in_data = ~in_data;
-	#32 in_PoR=1'b0; 				//Deshabilito el dispositivo
+	//#32 in_PoR=1'b0; 				//Deshabilito el dispositivo
 	#16 $finish;					//Finalizo la simulación
       end
  
