9c155ced5da8fed615b41c397eed4fa53a91571f
Agustin Miguel Grosso <agrosso@frba.utn.edu.ar>
2017-06-17 11:19:24 -0300

    Miller modificado 17-06

--- VERILOG/Miller Modificado/Miller Modif Deco/mill_modif_demod.v
+++ VERILOG/Miller Modificado/Miller Modif Deco/mill_modif_demod.v
@@ -1,5 +1,5 @@
 /*------------------------------------------------------------------------
---                     Decodificador Miller Modificado                 	--
+--                     Decodificador Miller Modificado              --
 -- * 1.0                                                          	--
 -- Descripcion:                                                   	--
 --    	Decodificador Miller Modificado	                          	--
@@ -7,67 +7,91 @@
 --	Test bench de este archivo en tb_mill_modif_demod.v		--
 ------------------------------------------------------------------------*/
 
-module mill_modif_demod #(parameter N = 4)(clk, in_PoR, in_data, out_data, in_pause);
-input clk;
-wire in_clk;				    //Clock de fc/4 en RFID (3.39 MHz)
-input in_data;			    //Informacion - ETU de 106Kb/s (32 clocks dentro de cada ETU)
-input in_PoR;		        //Power on reset
-output reg out_data;	  //Salida del decodificador miller modificado
-input in_pause;			    //Registro que viene desde el detector de pause
+module mill_modif_demod #(parameter N = 5)(clk, in_PoR, in_data, out_data, in_pause, out_pause);
+  input clk;					    //Clock de fc/4 en RFID (3.39 MHz)
+  wire in_clk;				    //Clock de fc/4 en RFID (3.39 MHz) modulado- Para la simulación
+  input in_data;			    //Informacion - ETU de 106Kb/s (32 clocks dentro de cada ETU)
+  input in_PoR;		        //Power on reset
+  input in_pause;			    //Registro que viene desde el detector de pause
+  output reg out_pause;		//Salida que resetea el detector  de pausa
+  output reg out_data;	  	//Salida del decodificador miller modificado
 
-reg  [N-1:0] reg_count;		//Cuento los clock para estar en la mitad del ETU
-reg reg_etu;				      //0 si se está en la primer mitad del etu - 1 si se está en la segunda mitad
-reg pre_out;			        //Usado para ir haciendole AND a cada bit que llega por in_data
-reg reg_pause;
+  reg  [N-1:0] reg_count;		//Cuento los clock para estar en la mitad del ETU
+  reg reg_mitad_etu;				//1 cuando se contó la primer mitad del ETU
+  reg reg_etu;              //1 cuando se contó un ETU completo
+  reg reg_flag;				      //Flag usado cuando viene primero la pausa y, luego, la mitad de ETU
+  reg flag_pause;				    //Usado para resetear el Módulo Detector de Pausa
   
 /*------------Quitar este assign. Es solamente para simulacion------------*/
   assign in_clk = (clk & in_data);  //Para simular cuando nos quedamos sin clock
   
-  always @ (negedge in_clk, negedge in_PoR, posedge in_pause) begin
+	
+	
+	always @(negedge in_clk, negedge in_PoR) begin
+		
+    if(~in_PoR) begin	//Cuando esta desabilitado el módulo, pongo la salida, los registros y el contador en 0 (cero)
+			out_data <= 1'b0;
+      out_pause <= 1'b0;
+			reg_etu <= 1'b0;
+      reg_mitad_etu <= 1'b0;
+      reg_count <= {N{1'b0}};
+			reg_flag <= 1'b0;
+			flag_pause <= 1'b0;
+		end else begin
+        	if(in_data) begin
+          		reg_count <= reg_count +1;  //Cuento los clock que entran en los ETUS
+        	end
       
+      //-Boque para detectar, mediante un contador, la mitad de un ETU y un ETU completo-
+      if(reg_count == {{(N-1){1'b1}},{1'b0}}) begin	//Cuando cuento los 32 clocks que entran en un etu
+        reg_etu <= 1'b1;			//Cambio a estado alto el registro	
+        reg_mitad_etu <= 1'b0;               
+			end else begin
+        if(reg_count == {{{{1'b0},{(N-3){1'b1}}},{1'b0}},{1'b1}}) begin	//Cuando cuento la mitad de los clocks que entran en un etu (tomo un margen)
+         reg_mitad_etu <= 1'b1;			//Cambio a estado alto el registro		          
+        end
+      end
+      //---------------------------------------------------------------------------------
       
-      if(~in_PoR) begin	//Cuando esta desabilitado el modulo, pongo la salida, los registros y el contador en 0 (cero)
-        out_data <= 1'b0;
-        reg_etu <= 1'b0;
-        reg_count <= {N{1'b0}};
-        pre_out <= 1'b1;
-        reg_pause <= 1'b0;
-      end else begin
-        if(in_pause) begin
- 	        reg_pause <= in_pause;
-        end
-        if(in_data) begin
-          reg_count <= reg_count +1;
-        end
-                 
-        
-  
-        if(reg_count == {N{1'b1}}) begin	//Cuando cuento la mitad de los clocks que entran en un etu
-          if(reg_pause) begin		//Hubo una pausa
-              if(~reg_etu) begin			//No se conto una primer mitad del ETU 
-                out_data <= 1'b0;		//Cero a la salida
-                reg_count <= {{(N-1){1'b0}},1'b1};		//reseteo el contador  
-                reg_etu = 1'b0;
-                reg_pause = 1'b0;
-              end
-          end else begin
-            if(~reg_etu) begin			//Es el primer ETU que cuento
-              reg_etu <= ~reg_etu;
-            end else begin          //Conte un ETU completo, es decir, un cero
-              reg_etu <= ~reg_etu;			//Reseteo el registro
-              out_data <= 1'b0;			//Cero a la salida        
-              reg_pause = 1'b0;      
-            end             
-            reg_count <= {{(N-1){1'b0}},1'b1};		//reseteo el contador 
-          end
-      end        
-        
-         if(reg_pause && reg_etu) begin
-        out_data <= 1'b1;
-        reg_count <= {{(N-1){1'b0}},1'b1};		//reseteo el contador
-        reg_etu <= ~reg_etu;			//Reseteo el registro
-        reg_pause = 1'b0;
-      end  
-    end
-  end
+      //-Bloque para resetear el módulo Detector de Pausa-//
+			if(in_pause) begin
+				if(~flag_pause) begin
+					flag_pause <= 1'b1;
+				end else begin
+					out_pause <= 1'b1;	//Reseteo el detector de pausa
+				end
+			end else begin
+				flag_pause <= 1'b0;
+				out_pause <= 1'b0;
+			end				
+      //--------------------------------------------------//
+		end
+	end
+	
+	
+  always @(posedge flag_pause or posedge reg_etu or posedge reg_mitad_etu) begin
+		
+    if(reg_mitad_etu & reg_flag)  begin
+			out_data <= 1'b0;
+			reg_flag <= 1'b0;
+			reg_mitad_etu<= ~ reg_mitad_etu;
+      reg_count <= {{(N-1){1'b0}},1'b1};
+		end	
+		if(flag_pause) begin		//Hubo una pausa
+      if(reg_mitad_etu) begin	    //Hubo una primer mitad del ETU
+				out_data <= 1'b1;		//Uno a la salida para resetear el Detector de Pausa
+			  reg_mitad_etu<= ~ reg_mitad_etu;
+				reg_flag <= 1'b0;
+        reg_count <= {{(N-1){1'b0}},1'b1};
+			end else begin 		    //Vino primero la pausa que la mitad del ETU
+				reg_flag <= 1'b1;
+			end
+		end else begin
+			if(reg_etu) begin	//Si conté un ETU completo
+				out_data <= 1'b0;
+        reg_etu <= ~reg_etu;
+        reg_count <= {{(N-1){1'b0}},1'b1};        
+			end
+		end
+	end
 endmodule
